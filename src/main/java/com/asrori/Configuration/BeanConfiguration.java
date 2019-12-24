f005f864c978f0c117fd7a217c1003df504abe5e
Ahmad Asrori <ahmad.asrori08@gmail.com>
2019-12-24 13:43:38 +0700

    initial commit

---
+++ src/main/java/com/asrori/Configuration/BeanConfiguration.java
@@ -0,0 +1,24 @@
+package com.asrori.Configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class BeanConfiguration {
+
+    @Bean
+    public DataSource dataSource(){
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("org.postgresql.Driver");
+        dataSource.setUrl("jdbc:postgresql://localhost:5432/bank");
+        dataSource.setUsername("postgres");
+        dataSource.setPassword("asrori08");
+        return dataSource;
+    }
+
+
+
+}
